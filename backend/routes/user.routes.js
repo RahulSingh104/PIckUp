eac9a557d4c70cef80d6de3a13788cfa3bc63454
RahulSingh104 <rahulsingh96287@gmail.com>
2026-02-26 20:19:27 +0530

    new commit change

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const {body} = require('express-validator');
 
+route.post('/register',[
+    body('email').isEmail().withMessage('please enter a valid email'),
+    body('fullname.firstName').isLength({min:3}).withMessage('first name must be at least 3 characters long'),
+])
 
 
 
