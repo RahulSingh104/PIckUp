34de695d9639911ddb7364d44e43725abf9bb611
RahulSingh104 <rahulsingh96287@gmail.com>
2026-03-02 20:38:08 +0530

    new soem changes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,7 +5,7 @@ const {body} = require('express-validator');
 
 route.post('/register',[
     body('email').isEmail().withMessage('please enter a valid email'),
-    body('fullname.firstName').isLength({min:3}).withMessage('first name must be at least 3 characters long'),
+    body('fullname.firstName').isLength({min:3}).withMessage('first name must be at least 3 characters a long'),
 ])
 
 
